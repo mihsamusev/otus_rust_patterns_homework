f9dd76515ff697262930d9a86253bcfed3d2350c
mihsamusev <mhs@bluepp.dk>
2022-08-26 10:38:25 +0200

    almost done, missing cleanup

--- examples/dynamic_command_loop.rs
+++ examples/dynamic_command_loop.rs
@@ -2,12 +2,13 @@
 
 use std::collections::VecDeque;
 use std::{cell::RefCell, rc::Rc};
+use std::thread;
 
 #[derive(thiserror::Error, Debug)]
 pub enum CommandError {
-    #[error("RepeatError: repetition failed")]
+    #[error("RepeatError: repetition failed - {0}")]
     RepeatError(String),
-    #[error("CommandError: execution failed")]
+    #[error("CommandError: execution failed - {0}")]
     CommandError(String),
 }
 
@@ -15,14 +16,59 @@ trait Command {
     fn execute(&mut self) -> Result<(), CommandError>;
 }
 
-type CommandRef = Rc<RefCell<dyn Command>>;
+type CommandRef = Box<dyn Command>;
 type CommandQueueRef = Rc<RefCell<VecDeque<CommandRef>>>;
 
-struct PrintNumber {
+struct LinearBody {
+    position: (f32, f32),
+    velocity: (f32, f32),
+}
+
+impl LinearBody { 
+    fn new(position: (f32, f32), velocity: (f32, f32)) -> Self {
+        Self {position, velocity}
+    }
+    fn update_state(&mut self) {
+        self.position = (self.position.0 + self.velocity.0, self.position.1 + self.velocity.1);
+    }
+
+    fn set_velocity(&mut self, velocity: (f32, f32)) {
+        self.velocity = velocity;
+    }
+
+    fn describe_state(&self) -> String {
+        format!("Body is at position {:?}, with velocity {:?}", self.position, self.velocity)
+    }
+}
+
+struct MoveStraightCommand {
+    body: Rc<RefCell<LinearBody>>
+}
+
+impl Command for MoveStraightCommand {
+    fn execute(&mut self) -> Result<(), CommandError> {
+        self.body.borrow_mut().update_state();
+        Ok(())
+    }
+}
+
+struct ChangeVelocityCommand {
+    body: Rc<RefCell<LinearBody>>,
+    new_velocity: (f32, f32)
+}
+
+impl Command for ChangeVelocityCommand {
+    fn execute(&mut self) -> Result<(), CommandError> {
+        self.body.borrow_mut().set_velocity(self.new_velocity);
+        Ok(())
+    }
+}
+
+struct PrintNumberCommand {
     number: f32,
 }
 
-impl Command for PrintNumber {
+impl Command for PrintNumberCommand {
     fn execute(&mut self) -> Result<(), CommandError> {
         println!("Number is {}", self.number);
         Ok(())
@@ -35,24 +81,7 @@ struct RepeatCommand {
 
 impl Command for RepeatCommand {
     fn execute(&mut self) -> Result<(), CommandError> {
-        self.inner
-            .borrow_mut()
-            .execute()
-            .map_err(|e| CommandError::RepeatError(e.to_string()))
-    }
-}
-
-struct EnqueueCommand {
-    inner: CommandRef,
-    command_queue: CommandQueueRef,
-}
-
-impl Command for EnqueueCommand {
-    fn execute(&mut self) -> Result<(), CommandError> {
-        self.command_queue
-            .borrow_mut()
-            .push_back(self.inner.clone());
-        Ok(())
+        self.inner.execute().map_err(|e| CommandError::RepeatError(e.to_string()))
     }
 }
 
@@ -61,7 +90,7 @@ struct FailCommand {}
 impl Command for FailCommand {
     fn execute(&mut self) -> Result<(), CommandError> {
         // import randomness, if true crash if not Ok
-        Err(CommandError::CommandError("fail".to_string()))
+        Err(CommandError::CommandError("I failed".to_string()))
     }
 }
 
@@ -92,12 +121,10 @@ struct RetryThenPanicErrorHandler {
 impl ErrorHandler for RetryThenPanicErrorHandler {
     fn handle(&mut self, command: CommandRef, error: CommandError) {
         match error {
-            CommandError::CommandError(_) => EnqueueCommand {
-                inner: command,
-                command_queue: self.command_queue.clone(),
-            }
-            .execute()
-            .unwrap(),
+            CommandError::CommandError(e) => {
+                println!("CommandError: {} -> gonna retry", e);
+                self.command_queue.borrow_mut().push_back(Box::new(RepeatCommand{inner: command}))
+            },
             CommandError::RepeatError(e) => {
                 panic!("Retried once, still failure: {}", e)
             }
@@ -107,45 +134,55 @@ impl ErrorHandler for RetryThenPanicErrorHandler {
 
 fn execute_commands<E: ErrorHandler>(command_queue: CommandQueueRef, error_handler: &mut E) {
     while command_queue.borrow().len() > 0 {
-        let command = command_queue.borrow_mut().pop_front().unwrap();
-        match command.borrow_mut().execute() {
+        println!("commands left {}", command_queue.borrow().len());
+        let mut command = command_queue.borrow_mut().pop_front().unwrap();
+        match command.execute() {
             Ok(_) => {}
-            Err(err) => error_handler.handle(command.clone(), err),
+            Err(err) => error_handler.handle(command, err),
         };
+        thread::sleep_ms(1000);
+        //command.execute();
+    }
+}
+
+fn execute_body_commands<E: ErrorHandler>(body: Rc<RefCell<LinearBody>>, command_queue: CommandQueueRef, error_handler: &mut E) {
+    while command_queue.borrow().len() > 0 {
+        println!("Current state: {}", body.borrow().describe_state());
+        println!("Commands left: {}", command_queue.borrow().len());
+        let mut command = command_queue.borrow_mut().pop_front().unwrap();
+        match command.execute() {
+            Ok(_) => {}
+            Err(err) => error_handler.handle(command, err),
+        };
+        thread::sleep_ms(1000);
         //command.execute();
     }
 }
 
 fn main() {
-    let first_command = Rc::new(RefCell::new(PrintNumber { number: 10.0 }));
-    let repeater = RepeatCommand {
-        inner: first_command.clone(),
-    };
     let command_queue: CommandQueueRef = Rc::new(RefCell::new(VecDeque::new()));
-    let enqueuer = EnqueueCommand {
-        inner: first_command.clone(),
-        command_queue: command_queue.clone(),
-    };
+    command_queue.borrow_mut().push_back(Box::new(PrintNumberCommand { number: 10.0 }));
+    command_queue.borrow_mut().push_back(Box::new(PrintNumberCommand { number: 15.0 }));
+    command_queue.borrow_mut().push_back(Box::new(FailCommand {}));
+    command_queue.borrow_mut().push_back(Box::new(PrintNumberCommand { number: 20.0 }));   
 
-    command_queue.borrow_mut().push_back(first_command);
-    command_queue
-        .borrow_mut()
-        .push_back(Rc::new(RefCell::new(repeater)));
-    command_queue
-        .borrow_mut()
-        .push_back(Rc::new(RefCell::new(enqueuer)));
-    command_queue
-        .borrow_mut()
-        .push_back(Rc::new(RefCell::new(FailCommand {})));
-    command_queue
-        .borrow_mut()
-        .push_back(Rc::new(RefCell::new(PrintNumber { number: 20.0 })));
-
-    //let mut error_handler = PrinterErrorHandler {}; // just print the error
-    let mut error_handler = PanicErrorHandler {}; // just crash
-                                                  //let mut error_handler = RetryThenPanicErrorHandler {
-                                                  // try again then crash
-                                                  //command_queue: command_queue.clone(),
-                                                  //};
+    let mut error_handler = PrinterErrorHandler {}; // just print the error
+    // let mut error_handler = PanicErrorHandler {}; // just crash
+    // let mut error_handler = RetryThenPanicErrorHandler {command_queue: command_queue.clone()};
     execute_commands(command_queue, &mut error_handler);
+
+
+    // example 2
+    let body = Rc::new(RefCell::new(LinearBody::new((0.0, 0.0), (1.0, 1.0))));
+    let command_queue: CommandQueueRef = Rc::new(RefCell::new(VecDeque::new()));
+    command_queue.borrow_mut().push_back(Box::new(MoveStraightCommand {body: body.clone()}));
+    command_queue.borrow_mut().push_back(Box::new(MoveStraightCommand {body: body.clone()}));
+    command_queue.borrow_mut().push_back(Box::new(ChangeVelocityCommand {body: body.clone(), new_velocity: (0.5, 1.5)}));
+    command_queue.borrow_mut().push_back(Box::new(MoveStraightCommand {body: body.clone()}));
+    command_queue.borrow_mut().push_back(Box::new(MoveStraightCommand {body: body.clone()}));
+
+    let mut error_handler = PrinterErrorHandler {}; // just print the error
+    // let mut error_handler = PanicErrorHandler {}; // just crash
+    // let mut error_handler = RetryThenPanicErrorHandler {command_queue: command_queue.clone()};
+    execute_body_commands(body.clone(), command_queue, &mut error_handler);
 }
